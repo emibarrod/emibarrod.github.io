4726b26d03883a7410836575358595b5319af600
emibarrod <emibarrod@gmail.com>
2024-01-04 18:27:09 +0100

    add auto year and keywords

--- navbar-button.js
+++
@@ -1,5 +0,0 @@
-// Función para mostrar/ocultar la navbar en dispositivos móviles
-document.getElementById('toggle-btn').addEventListener('click', function () {
-    var nav = document.querySelector('nav ul');
-    nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
-});
