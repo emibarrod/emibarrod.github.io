0fa34c2359d62ab863b4c28959e9cf1be29fa701
emibarrod <emibarrod@gmail.com>
2025-09-02 16:53:59 +0200

    migrate web to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://emibarrod.com',
+	site: 'https://emibarrod.github.io',
 	integrations: [mdx(), sitemap()],
 });
