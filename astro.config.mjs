8afdbfdf29cf9f91ef66ff548863eedac86a2f76
emibarrod <emibarrod@gmail.com>
2025-09-02 16:48:09 +0200

    fix: Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://emibarrod.com',
+	site: 'https://emibarrod.github.io',
+	base: '/emibarrod.github.io',
 	integrations: [mdx(), sitemap()],
 });
