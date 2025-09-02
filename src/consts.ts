1b79164867729d7ca60f87a0652773c31df03165
emibarrod <emibarrod@gmail.com>
2025-09-02 16:42:45 +0200

    migrate web to astro

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Emilio Barragán';
+export const SITE_DESCRIPTION = 'Blog personal de Emilio Barragán. Posts sobre ingeniería de datos, python y más.';
